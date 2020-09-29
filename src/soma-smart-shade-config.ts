1a372dcce158bb2c46cc64303e8e4be0136896be
Giles Roadnight <10414642+Roaders@users.noreply.github.com>
2020-09-29 09:44:44 +0100

    Implement soma shade node

--- src/soma-smart-shade-config.ts
+++ src/soma-smart-shade-config.ts
@@ -1,4 +1,6 @@
 import { Node, NodeProperties, Red } from 'node-red';
+import {Request, Response, Application} from "express"
+import axios from "axios"
 
 export interface ISomaSmartShadeProperties extends NodeProperties {
     port: number;
@@ -23,4 +25,11 @@ module.exports = function (module: Red) {
     }
 
     module.nodes.registerType('soma-smart-shade-config', SomaSmartShadeConfig);
+
+    (module.httpNode as Application).get('/soma-smart-shade-devices', async (req: Request, res: Response) => {
+        const baseUrl = getBaseUrl(req.query as any);
+        const result = await axios.get(`${baseUrl}/list_devices`);
+
+        res.json(result.data.shades);
+    });
 };
--- src/soma-smart-shade.ts
+++ src/soma-smart-shade.ts
@@ -1,21 +1,92 @@
 import { Red, Node, NodeProperties } from 'node-red';
 import { ISomaSmartShadeConfigNode } from './soma-smart-shade-config';
+import axios from "axios";
 
 export interface ISomaSmartShadeProperties extends NodeProperties {
-    server: string;
+    server?: string;
+    blindid?: string;
 }
 
 interface ISomaSmartShadeNode extends Node {
-    server: ISomaSmartShadeConfigNode | null;
+    server?: ISomaSmartShadeConfigNode | null;
 }
+
+enum NodeAction {
+    open = "open",
+    close = "close",
+    stop = "stop",
+    getPosition = "getPosition",
+    setPosition = "setPosition",
+    getBattery = "getBattery",
+}
+
+type BlindPayload = { action: keyof typeof NodeAction, position?: number | string };
 
 module.exports = function (module: Red) {
     function SomaSmartShadeNode(this: ISomaSmartShadeNode, config: ISomaSmartShadeProperties) {
         module.nodes.createNode(this, config);
 
-        this.server = module.nodes.getNode(config.server) as ISomaSmartShadeConfigNode;
+        this.server = config.server ? module.nodes.getNode(config.server) as ISomaSmartShadeConfigNode : undefined;
+
+        this.on('input', async (msg, send, done) => {
+            const payload: BlindPayload = typeof msg.payload === "string" ? {action: msg.payload} : msg.payload;
+
+            if (typeof payload.action != "string" || NodeAction[payload.action] == null) {
+                this.warn(`Incorrect msg.payload received: '${JSON.stringify(msg.payload)}'. Payload must be one of ${Object.keys(NodeAction).map(a => `'${a}'`).join(', ')} or an object containing the action: { action: "open"}`);
+                return;
+            }
+
+            if (this.server == null || config.blindid == null) {
+                return;
+            }
+
+            let url: string;
+
+            switch (payload.action) {
+                case "open":
+                    url = `${this.server.serverAddress}/open_shade/${config.blindid}`;
+                    break;
+                case "close":
+                    url = `${this.server.serverAddress}/close_shade/${config.blindid}`;
+                    break;
+                case "stop":
+                    url = `${this.server.serverAddress}/stop_shade/${config.blindid}`;
+                    break;
+                case "getPosition":
+                    url = `${this.server.serverAddress}/get_shade_state/${config.blindid}`;
+                    break;
+                case "setPosition":
+                    if(payload.position == null){
+                        this.warn(`Incorrect msg.payload received: '${JSON.stringify(msg.payload)}'. To set position the following object must be sent: {action: "setPosition", position: 30}`);
+
+                        return;
+                    }
+                    url = `${this.server.serverAddress}/set_shade_position/${config.blindid}/${payload.position}`;
+                    break;
+                case "getBattery":
+                    url = `${this.server.serverAddress}/get_battery_level/${config.blindid}`;
+                    break;
+                default:
+                    handleNever(this, payload.action);
+                    return;
+            }
+
+            axios.get(url).then(result => {
+                msg.payload = result.data;
+
+                send(msg);
+            }).catch(err => {
+                done(err);
+            })
+
+        });
 
     }
 
     module.nodes.registerType('soma-smart-shade', SomaSmartShadeNode);
 };
+
+function handleNever(node: ISomaSmartShadeNode, action: never) {
+
+    node.warn(`Unknown payload: ${action}`);
+}
